d97ebd876d2d8a7fc70f1f927dc9dbd2998d42b5
muyiwer <43145915+muyiwer@users.noreply.github.com>
2021-04-08 09:40:29 +0100

    Completed first assignment

--- cypress/integration/google/google.spec.js
+++ cypress/integration/google/google.spec.js
@@ -1,12 +1,19 @@
-context('Google', () => {
-    describe('Vist the paystack home page', () => {
-        beforeEach(() => {
-            cy.visit('https://google.com');
-            cy.viewport(1440, 900);
-        })
-
-        it('Should login a user successfully', () => {
-            cy.get('input.gLFyf').type('Celestine Omin{enter}');
-        })
+context("Yandex", () => {
+  describe("Vist Yandex home page", () => {
+    beforeEach(() => {
+      cy.visit("https://mail.yandex.com/");
+      cy.viewport(1440, 900);
     });
+
+    it("Should Page Load successfully", () => {
+      cy.wait(5000);
+      cy.contains("Log in").click();
+      cy.get("input.Textinput-Control").first().type("kazlanre{Enter}");
+      cy.get("input[name='passwd']").last().type("bLErKm9H@@VvCS8{Enter}");
+      cy.wait(5000);
+      cy.get("a.mail-MessageSnippet").first().click();
+      cy.wait(5000);
+      cy.contains("Inbox").click();
+    });
+  });
 });
